dd662769462d87548e3efce8632032e7ff89f6ee
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2023-12-13 21:40:48 +0100

    refactoring

--- 13/puzzle13.py
+++ 13/puzzle13.py
@@ -1,13 +1,11 @@
 import functools
 import math
 
-def change_smudge(field, allowed_mistakes):
+def find_solution(field, allowed_mistakes):
+    # Rows
     for i in range(len(field)-1):
         rows_top = [x for x in range(i, -1,-1)]
         rows_bottom = [x for x in range(i+1, len(field))]
-        min_len = min(len(rows_top), len(rows_bottom))
-        rows_top = rows_top[:min_len]
-        rows_bottom = rows_bottom[:min_len]
 
         faults = 0
         for x,y in zip(rows_bottom, rows_top):
@@ -17,13 +15,11 @@ def change_smudge(field, allowed_mistakes):
         if faults == allowed_mistakes:
             return (i+1)*100
     
+    # Columns
     for i in range(len(field[0])-1):
         columns_top = [x for x in range(i, -1,-1)]
         columns_bottom = [x for x in range(i+1, len(field[0]))]
-        min_len = min(len(columns_top), len(columns_bottom))
-        columns_top = columns_top[:min_len]
-        columns_bottom = columns_bottom[:min_len]
-
+        
         faults = 0
         for x,y in zip(columns_bottom, columns_top):
             for j, row in enumerate(field):
@@ -39,8 +35,8 @@ with open('13/input.txt', 'r') as f:
     total = 0
     total2 = 0
     for field in input:
-        total += change_smudge(field,0)
-        total2 += change_smudge(field,1)
+        total += find_solution(field,0)
+        total2 += find_solution(field,1)
     print(total, total2)
     
     

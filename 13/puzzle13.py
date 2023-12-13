90481bb726abbf7c11c9a68658881859c54a4ec4
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2023-12-13 21:21:53 +0100

    day 13 part2

--- 13/puzzle13.py
+++ 13/puzzle13.py
@@ -1,83 +1,46 @@
 import functools
 import math
 
-with open('13/testinput.txt', 'r') as f:
+def change_smudge(field, allowed_mistakes):
+    for i in range(len(field)-1):
+        rows_top = [x for x in range(i, -1,-1)]
+        rows_bottom = [x for x in range(i+1, len(field))]
+        min_len = min(len(rows_top), len(rows_bottom))
+        rows_top = rows_top[:min_len]
+        rows_bottom = rows_bottom[:min_len]
+
+        faults = 0
+        for x,y in zip(rows_bottom, rows_top):
+            for j in range(len(field[x])):
+                if field[x][j] != field[y][j]:
+                    faults += 1
+        if faults == allowed_mistakes:
+            return (i+1)*100
+    
+    for i in range(len(field[0])-1):
+        columns_top = [x for x in range(i, -1,-1)]
+        columns_bottom = [x for x in range(i+1, len(field[0]))]
+        min_len = min(len(columns_top), len(columns_bottom))
+        columns_top = columns_top[:min_len]
+        columns_bottom = columns_bottom[:min_len]
+
+        faults = 0
+        for x,y in zip(columns_bottom, columns_top):
+            for j, row in enumerate(field):
+                if row[x] != row[y]:
+                    faults += 1
+        if faults == allowed_mistakes:
+            return i+1
+
+with open('13/input.txt', 'r') as f:
     input = [line.split('\n') for line in f.read().split('\n\n')]
+    input = [[[x for x in line] for line in field]for field in input]
     
     total = 0
+    total2 = 0
     for field in input:
-        
-        for i in range(len(field)-1):
-            rows_top = [x for x in range(i, -1,-1)]
-            rows_bottom = [x for x in range(i+1, len(field))]
-            min_len = min(len(rows_top), len(rows_bottom))
-            rows_top = rows_top[:min_len]
-            rows_bottom = rows_bottom[:min_len]
-
-            symetric = True
-            for x,y in zip(rows_bottom, rows_top):
-                if field[x] != field[y]:
-                    symetric = False
-            if symetric:
-                total += (i+1)*100
-
-        
-        for i in range(len(field[0])-1):
-            columns_top = [x for x in range(i, -1,-1)]
-            columns_bottom = [x for x in range(i+1, len(field[0]))]
-            min_len = min(len(columns_top), len(columns_bottom))
-            columns_top = columns_top[:min_len]
-            columns_bottom = columns_bottom[:min_len]
-
-            symetric = True
-            for x,y in zip(columns_bottom, columns_top):
-                if any([row[x] != row[y] for row in field]):
-                    symetric = False
-            if symetric:
-                total += (i+1)
-    print(total)
-
-    #Part 2
-    total = 0
-    for field in input:
-        faults = 0
-
-        for i in range(len(field)-1):
-            rows_top = [x for x in range(i, -1,-1)]
-            rows_bottom = [x for x in range(i+1, len(field))]
-            min_len = min(len(rows_top), len(rows_bottom))
-            rows_top = rows_top[:min_len]
-            rows_bottom = rows_bottom[:min_len]
-
-            symetric = True
-            switch = ()
-            for x,y in zip(rows_bottom, rows_top):
-                for j in range(len(field[x])):
-                    if field[x][j] != field[y][j]:
-                        if faults == 0:
-                            faults += 1
-                            switch = (x,j)
-                        else: 
-                            symetric = False
-                if faults == 1:
-                    field[switch[0]][switch[1]] = '#' if field[switch[0]][switch[1]] =='.' else '.'
-            if symetric:
-                total += (i+1)*100
-
-        
-        for i in range(len(field[0])-1):
-            columns_top = [x for x in range(i, -1,-1)]
-            columns_bottom = [x for x in range(i+1, len(field[0]))]
-            min_len = min(len(columns_top), len(columns_bottom))
-            columns_top = columns_top[:min_len]
-            columns_bottom = columns_bottom[:min_len]
-
-            symetric = True
-            for x,y in zip(columns_bottom, columns_top):
-                if any([row[x] != row[y] for row in field]):
-                    symetric = False
-            if symetric:
-                total += (i+1)
-    print(total)
+        total += change_smudge(field,0)
+        total2 += change_smudge(field,1)
+    print(total, total2)
     
     
